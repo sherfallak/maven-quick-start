e6d95e561cfd9fff5c80f28ef9a01544e15781eb
Fallak Sher <sh.fallak@gmail.com>
2018-08-19 00:37:21 +0200

    changing plugin to java source 1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,7 +1,16 @@
 package doctor.programming.training;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     
+	public void greet(){
+		List<String> greetings=new ArrayList<>();
+		greetings.add("Hello");
+		for(String greeting:greetings){
+			System.out.println("Greeting:"+greeting);
+		}
+	}
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -10,5 +19,6 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	app.greet();
     }
 }
